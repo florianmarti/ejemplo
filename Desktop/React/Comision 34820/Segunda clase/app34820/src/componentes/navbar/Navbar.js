a62360a94f430024ab3b333f5e6d8e18158be11f
Florian <florianmarti@gmail.com>
2022-11-15 13:24:46 -0300

    Agregado de entrega navbar

---
+++ Desktop/React/Comision 34820/Segunda clase/app34820/src/componentes/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css'
+const Navbar = ()=>{
+    return(
+    <div className='divConteiner'> 
+        <nav className='containerNav'> 
+            <h1>Navbar de eCommerce</h1>
+            <button className="boton">Boton 1</button>
+            <button className='boton'>Boton 2</button>
+        </nav>
+    </div>
+    )
+}
+export default Navbar
